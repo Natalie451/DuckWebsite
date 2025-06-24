57684c196eccfefe068ef9fd9dbb7fe732e7c750
Natalie451 <81078733+Natalie451@users.noreply.github.com>
2025-06-24 22:54:16 +0100

    Added Event Listener. Now the confirm button works

--- mockscript.js
+++ mockscript.js
@@ -1,1 +1,14 @@
 /* Mock Lab Practical */
+var realNameInput = document.getElementById("realName");
+var duckNicknameInput = document.getElementById("duckNickname");
+var confirmBtn = document.getElementById("confirmSelection");
+var finalNicknameField = document.getElementById("finalNickname");
+
+//Event listener that makes the confirm button work.
+//Whenever the the confirm button is clicked, the name entered
+// in your name input and duck input all transfer to the final field.
+confirmBtn.addEventListener("click", function () {
+  var realName = realNameInput.value;
+  var duckNickname = duckNicknameInput.value;
+  finalNicknameField.value = realName + " " + duckNickname;
+});
