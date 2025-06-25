99fe9ef3a5bf856a221cfeb428ae887b765776bf
Natalie451 <81078733+Natalie451@users.noreply.github.com>
2025-06-25 17:02:14 +0100

    Completed Task 3, Exercise 1. Added validation rules

--- mockscript.js
+++ mockscript.js
@@ -3,6 +3,7 @@ var realNameInput = document.getElementById("realName");
 var duckNicknameInput = document.getElementById("duckNickname");
 var confirmBtn = document.getElementById("confirmSelection");
 var finalNicknameField = document.getElementById("finalNickname");
+var nickNameFeedbackField = document.getElementById("nicknameFeedback");
 
 //Event listener that makes the confirm button work.
 //Whenever the the confirm button is clicked, the name entered
@@ -10,5 +11,31 @@ var finalNicknameField = document.getElementById("finalNickname");
 confirmBtn.addEventListener("click", function () {
   var realName = realNameInput.value;
   var duckNickname = duckNicknameInput.value;
-  finalNicknameField.value = realName + " " + duckNickname;
+  finalNicknameField.value = realName + "" + duckNickname;
+  validateNickname(realName, duckNickname);
 });
+
+//Validation rules for the name and nickname fields
+// This function will check the mumber of characters and spaces.
+//It will turn the text red if validation rules are broken.
+function validateNickname(realName, duckNickname) {
+  if (realName === "" || duckNickname === "") {
+    nickNameFeedbackField.innerHTML =
+      "Please fill in both your name and the duck nickname";
+    nickNameFeedbackField.style.color = "red";
+    return false;
+  } else if (
+    duckNickname.length < 4 ||
+    duckNickname.indexOf(" ") !== -1 ||
+    duckNickname.toLowerCase() === realName.toLowerCase()
+  ) {
+    nickNameFeedbackField.innerHTML =
+      "Nickname must be 4+ characters, no spaces, and not the same as your real name";
+    nickNameFeedbackField.style.color = "red";
+    return false;
+  } else {
+    nickNameFeedbackField.innerHTML = "\u2714 Valid nickname";
+    nickNameFeedbackField.style.color = "green";
+    return true;
+  }
+}
